edbe56e20a7c19f53d9a4e0fedca88e59fbcb32b
samshal <samueladeshina73@gmail.com>
2016-08-24 12:10:59 +0100

    Automated Push

--- src/Patients/bin/schema.sql
+++ src/Patients/bin/schema.sql
@@ -23,3 +23,14 @@ CREATE TABLE Patients.PatientTransaction(
 	FOREIGN KEY (PatientID) REFERENCES Patients.Patient(PatientID) ON UPDATE CASCADE ON DELETE CASCADE
 )
 GO
+
+CREATE TABLE Patients.PatientRepository (
+	RepositoryItemID INT PRIMARY KEY IDENTITY NOT NULL,
+	PatientID INT,
+	RepositoryItemNumber VARCHAR(50) NOT NULL UNIQUE,
+	RepositoryItemName VARCHAR(100),
+	RepositoryItemDescription VARCHAR(4000),
+	RepositoryItemUrl VARCHAR(MAX),
+	FOREIGN KEY (PatientID) REFERENCES Patients.Patient(PatientID) ON UPDATE CASCADE ON DELETE CASCADE
+)
+GO
