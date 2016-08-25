1f278996be9a58c269be47b658fc0dc1c3e71091
samshal <samueladeshina73@gmail.com>
2016-08-25 05:36:01 +0100

    Automated Push

--- src/Pharmacy/Dispensation/Dispensation.php
+++ src/Pharmacy/Dispensation/Dispensation.php
@@ -38,7 +38,7 @@ class Dispensation
     public static function create(array $data)
     {
        // $DispensedItem = $data['eligibleDispensories'] ?? null;
-        $dispensedItem = $data['dispensedItem'] ?? null;
+        $dispensedItem = $data['dispensedItems'] ?? null;
         $dispenseeType = $data['dispenseeType'] ?? null;
         $dispenseeTypeId = $data['dispenseeTypeId'] ?? null;
         $eligibleDispensory = $data['eligibleDispensory'] ?? null;
