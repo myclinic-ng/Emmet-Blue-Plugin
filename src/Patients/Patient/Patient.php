b9cb86379a36a7a72722aa4146933ca19291c8f3
samshal <samueladeshina73@gmail.com>
2016-08-27 12:39:35 +0100

    Automated Push

--- src/Patients/Patient/Patient.php
+++ src/Patients/Patient/Patient.php
@@ -207,6 +207,13 @@ class Patient
 
             $query = "SELECT * FROM Patients.PatientRecordsFieldValue WHERE PatientID = $resourceId";
 
+            $path = $viewPatients[0]['PatientIdentificationDocumentUrl']."/0.jpg";
+            $type = pathinfo($path, PATHINFO_EXTENSION);
+            $data = file_get_contents($path);
+            $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+
+            $viewPatients["PatientIdentificationDocumentUrl"][] = $base64;
+
             $viewPatientsRecords = (
                 DBConnectionFactory::getConnection()
                 ->query($query)
