35a492fe139e7c1e27806ea3b55f8ab127fd9d6b
samshal <samueladeshina73@gmail.com>
2016-08-28 22:35:27 +0100

    Automated Push

---
+++ src/Patients/Patient/HospitalHistory.php
@@ -0,0 +1,70 @@
+<?php declare(strict_types=1);
+/**
+ * @license MIT
+ * @author Bardeson Lucky <flashup4all@gmail.com>
+ *
+ * This file is part of the EmmetBlue project, please read the license document
+ * available in the root level of the project
+ */
+namespace EmmetBlue\Plugins\Patients\Patient;
+
+use EmmetBlue\Core\Builder\BuilderFactory as Builder;
+use EmmetBlue\Core\Factory\DatabaseConnectionFactory as DBConnectionFactory;
+use EmmetBlue\Core\Factory\DatabaseQueryFactory as DBQueryFactory;
+use EmmetBlue\Core\Builder\QueryBuilder\QueryBuilder as QB;
+use EmmetBlue\Core\Exception\SQLException;
+use EmmetBlue\Core\Session\Session;
+use EmmetBlue\Core\Logger\DatabaseLog;
+use EmmetBlue\Core\Logger\ErrorLog;
+use EmmetBlue\Core\Constant;
+
+use EmmetBlue\Plugins\Permission\Permission as Permission;
+
+/**
+ * class PatientHospitalHistory.
+ *
+ * PatientHospitalHistory Controller
+ *
+ * @author Samuel Adeshina <Samueladeshina73@gmail.com>
+ * @since v0.0.1 26/08/2016 12:33
+ */
+class HospitalHistory
+{
+    /**
+     * view patients UUID
+     */
+    public static function view(int $patientId)
+    {
+        $selectBuilder = (new Builder('QueryBuilder','Select'))->getBuilder();
+        $selectBuilder
+            ->columns('*')
+            ->from('Patients.PatientHospitalHistory');
+        if ($resourceId != 0){
+            $selectBuilder->where('PatientID ='.$resourceId);
+        }
+        try
+        {
+            $viewPatients = (DBConnectionFactory::getConnection()->query((string)$selectBuilder))->fetchAll(\PDO::FETCH_ASSOC);
+
+            DatabaseLog::log(
+                Session::get('USER_ID'),
+                Constant::EVENT_SELECT,
+                'Patients',
+                'PatientHospitalHistory',
+                (string)serialize($selectBuilder)
+            );
+
+            return $viewPatients;    
+        } 
+        catch (\PDOException $e) 
+        {
+            throw new SQLException(
+                sprintf(
+                    "Error processing request"
+                ),
+                Constant::UNDEFINED
+            );
+            
+        }
+    }
+}
