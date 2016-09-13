da0b84302e8a42f694c338ee6bb3c3eab529b0e5
samshal <samueladeshina73@gmail.com>
2016-09-13 04:57:16 +0100

    Automated Push

--- src/AccountsBiller/GetItemPrice.php
+++ src/AccountsBiller/GetItemPrice.php
@@ -34,10 +34,84 @@ class GetItemPrice
 
 		$query = "SELECT PatientType FROM Patients.Patient WHERE PatientID = $patient";
 		$patientType = (DBConnectionFactory::getConnection()->query($query))->fetchall(\PDO::FETCH_ASSOC)[0]["PatientType"];
+		if (is_null($patientType)){
+			throw new \Exception("Patient's Category has no associated price structure for the specified item");
+		}
 
 		$query = "SELECT * FROM Accounts.BillingTypeItemsPrices WHERE BillingTypeItem = $item AND PatientType = $patientType";
-		$result = (DBConnectionFactory::getConnection()->query($query))->fetchall(\PDO::FETCH_ASSOC)[0];
+		$result = (DBConnectionFactory::getConnection()->query($query))->fetchall(\PDO::FETCH_ASSOC);
+		if (empty($result)){
+			throw new \Exception("Price structure does not exist for the specified item");
+		}
 
-		return $result;
+		$result = $result[0];
+		$price = $result["BillingTypeItemPrice"];
+		if ($result["IntervalBased"]){
+			$query = "SELECT * FROM Accounts.BillingTypeItemsInterval WHERE BillingTypeItemID = $item";
+			$results = (DBConnectionFactory::getConnection()->query($query))->fetchall(\PDO::FETCH_ASSOC);
+			if (empty($result)){
+				throw new \Exception("Interval definition not found.");
+			}
+			$totalPrice = 0;
+			foreach ($results as $result) {
+				$interval = $result["Interval"];
+				$type = $result["IntervalIncrementType"];
+				$increment = $result["IntervalIncrement"];
+
+				$totalPrice += self::calculateIntervalBasedPrice($price, $quantity, $interval, $type, $increment);
+			}
+		}
+		else {
+			$totalPrice = self::calculateNonIntervalBasedPrice((int)$price, (int)$quantity);
+		}
+
+		return $totalPrice;
+	}
+
+	private static function calculateNonIntervalBasedPrice(int $price, int $quantity){
+		return $price * $quantity;
+	}
+
+	private static function calculateIntervalBasedPrice(int $price, int $quantity, int $interval, string $type, int $increment){
+		$totalPrice = $price;
+		switch (strtolower($type)){
+			case "additive":{
+				if ($quantity > $interval){
+					$dividend = $quantity / $interval;
+					$modulus = $quantity % $interval;
+					for ($i = 0; $i < $dividend; $i++){
+						$totalPrice += ($totalPrice + $increment) * $interval;
+					}
+					if ($modulus > 0){
+						$totalPrice += ($totalPrice + $increment) * $modulus;
+					}
+				}
+				break;
+			}
+
+			case "multiplicative":{
+				if ($quantity > $interval){
+					$dividend = $quantity / $interval;
+					$modulus = $quantity % $interval;
+					for($i = 0; $i < $dividend; $i++){
+						$totalPrice += ($totalPrice * $increment) * $interval;
+					}
+					if ($modulus > 0){
+						$totalPrice += ($totalPrice * $increment) * $modulus;
+					}
+				}
+				break;
+			}
+
+			case "custom":{
+				$counter = $quantity - $interval;
+				if ($counter > 0){
+					$totalPrice += ($totalPrice + $increment) * $counter;
+				}
+				break;
+			}
+		}
+
+		return $totalPrice;
 	}
 }
