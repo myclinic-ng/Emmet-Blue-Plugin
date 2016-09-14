7245f62f09fc6be511120274f6f47b3c38f4d6ab
samshal <samueladeshina73@gmail.com>
2016-09-14 04:45:09 +0100

    Automated Push

--- src/AccountsBiller/AccountsBillingTypeItems/ViewAccountsBillingTypeItems.php
+++ src/AccountsBiller/AccountsBillingTypeItems/ViewAccountsBillingTypeItems.php
@@ -108,6 +108,9 @@ class ViewAccountsBillingTypeItems
     public static function isRateBased(int $patientId, array $data){
         $item = $data["item"] ?? null;
 
+        $q = "SELECT * FROM PatientType FROM Patients.Patient WHERE PatientID = $patientId";
+        $type = (DBConnectionFactory::getConnection()->query($query))->fetchAll(\PDO::FETCH_ASSOC)[0]["PatientType"];
+
         $query = "SELECT * FROM Accounts.BillingTypeItemsPrices WHERE BillingTypeItem=$item AND PatientType=$patientId";
         $result = (DBConnectionFactory::getConnection()->query($query))->fetchAll(\PDO::FETCH_ASSOC);
 
