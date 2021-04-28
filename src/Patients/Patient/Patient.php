a192c3c93875e5de8744e78e2daf5b47a0de82df
samshal <samueladeshina73@gmail.com>
2021-04-28 21:29:05 +0100

    Search should show patients that visit if query is empty

--- src/Patients/Patient/Patient.php
+++ src/Patients/Patient/Patient.php
@@ -529,12 +529,10 @@ class Patient
     }
 
     private static function viewPatientsByCurrentDayVisit(){
-        $start = (new \DateTime())->format("d/m/Y");
-        $end = (new \DateTime("+1day"))->format("d/m/Y");
+        $start = (new \DateTime())->format("m/d/Y");
+        $end = (new \DateTime("+1day"))->format("m/d/Y");
 
         $query = "SELECT c.PatientID FROM Patients.PatientProfileUnlockLog a INNER JOIN Patients.Patient c ON a.PatientID = c.PatientID WHERE CONVERT(date, a.DateLogged) BETWEEN '$start' AND '$end'";
-
-        die($query);
 
         $results = DBConnectionFactory::getConnection()->query($query)->fetchAll(\PDO::FETCH_ASSOC);
 
