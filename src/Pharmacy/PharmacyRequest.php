6e7d848b34fbf76e9b372488e2862d999c0b421d
samshal <samueladeshina73@gmail.com>
2021-08-06 14:54:44 +0100

    d

--- src/Pharmacy/PharmacyRequest.php
+++ src/Pharmacy/PharmacyRequest.php
@@ -52,9 +52,9 @@ class PharmacyRequest
 		return PharmacyRequest\PharmacyRequest::close($resourceId, $data);
 	}
 
-	public static function declineRequest(array $data=[])
+	public static function declineRequest(int $resourceId, array $data=[])
 	{
-		return PharmacyRequest\PharmacyRequest::declineRequest($data);
+		return PharmacyRequest\PharmacyRequest::declineRequest($resourceId,$data);
 	}
 
 	public static function smartify(array $data)
