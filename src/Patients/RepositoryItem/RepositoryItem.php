7141ff12097dbfb572006f9397bdf61a1728edb0
samshal <samueladeshina73@gmail.com>
2021-09-25 08:34:58 +0100

    ..

--- src/Patients/RepositoryItem/RepositoryItem.php
+++ src/Patients/RepositoryItem/RepositoryItem.php
@@ -171,7 +171,7 @@ class RepositoryItem
                     case "file":{
                         $json = $data["file"] ?? null;
                         
-                        if (!self::createRepoFile($puuid, $ruuid, base64_decode($json), $number.".".$ext)){
+                        if (!self::createRepoFile($puuid, $ruuid, base64_decode($json, true), $number.".".$ext)){
                             self::delete((int)$result["lastInsertId"], $puuid, $number.".".$ext);
                         }
                         break;
